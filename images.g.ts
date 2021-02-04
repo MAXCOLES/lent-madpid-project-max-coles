4138c8d734090ff4319085eac04e140ece81f932
MAXCOLES <77454678+MAXCOLES@users.noreply.github.com>
2021-02-04 16:43:05 +0000

    tried to start making new levels

---
+++ images.g.ts
@@ -0,0 +1,106 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "image2":return img`
+.............ccfff..............
+............cddbbf..............
+...........cddbbf...............
+..........fccbbcf............ccc
+....ffffffccccccff.........ccbbc
+..ffbbbbbbbbbbbbbcfff.....cdbbc.
+ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+fbcbbbbbffbbcbcbbbcccccfffdbbf..
+fbbb1111ff1bcbcbbbcccccccbbbcf..
+.fb11111111bbbbbbcccccccccbccf..
+..fccc33cc11bbbbccccccccfffbbcf.
+...fc131c111bbbcccccbdbc...fbbf.
+....f33c111cbbbfdddddcc.....fbbf
+.....ff1111fbdbbfddcc........fff
+.......cccccfbdbbfc.............
+.............fffff..............
+`;
+            case "image3":return img`
+...........ccccc66666...........
+........ccc4444444444666........
+......cc444444444bb4444466......
+.....cb4444bb4444b5b444444b.....
+....eb4444b5b44444b44444444b....
+...ebb44444b4444444444b444446...
+..eb6bb444444444bb444b5b444446..
+..e6bb5b44444444b5b444b44bb44e..
+.e66b4b4444444444b4444444b5b44e.
+.e6bb444444444444444444444bb44e.
+eb66b44444bb444444444444444444be
+eb66bb444b5b44444444bb44444444be
+fb666b444bb444444444b5b4444444bf
+fcb666b44444444444444bb444444bcf
+.fbb6666b44444444444444444444bf.
+.efbb66666bb4444444444444444bfe.
+.86fcbb66666bbb44444444444bcc688
+8772effcbbbbbbbbbbbbbbbbcfc22778
+87722222cccccccccccccccc22226678
+f866622222222222222222222276686f
+fef866677766667777776667777fffef
+fbff877768f86777777666776fffffbf
+fbeffeefffeff7766688effeeeefeb6f
+f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+f66ddfffffeeeffeffeeeeeffeedb46e
+.c66ddd4effffffeeeeeffff4ddb46e.
+.fc6b4dddddddddddddddddddb444ee.
+..ff6bb444444444444444444444ee..
+....ffbbbb4444444444444444ee....
+......ffebbbbbb44444444eee......
+.........fffffffcccccee.........
+................................
+`;
+            case "image4":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,6 +1,49 @@
+function start_level () {
+    scene.setBackgroundColor(randint(3, 7))
+    count = 0
+    for (let level = 0; level <= 10 + level; level++) {
+        foods = sprites.create(img`
+            ...........ccccc66666...........
+            ........ccc4444444444666........
+            ......cc444444444bb4444466......
+            .....cb4444bb4444b5b444444b.....
+            ....eb4444b5b44444b44444444b....
+            ...ebb44444b4444444444b444446...
+            ..eb6bb444444444bb444b5b444446..
+            ..e6bb5b44444444b5b444b44bb44e..
+            .e66b4b4444444444b4444444b5b44e.
+            .e6bb444444444444444444444bb44e.
+            eb66b44444bb444444444444444444be
+            eb66bb444b5b44444444bb44444444be
+            fb666b444bb444444444b5b4444444bf
+            fcb666b44444444444444bb444444bcf
+            .fbb6666b44444444444444444444bf.
+            .efbb66666bb4444444444444444bfe.
+            .86fcbb66666bbb44444444444bcc688
+            8772effcbbbbbbbbbbbbbbbbcfc22778
+            87722222cccccccccccccccc22226678
+            f866622222222222222222222276686f
+            fef866677766667777776667777fffef
+            fbff877768f86777777666776fffffbf
+            fbeffeefffeff7766688effeeeefeb6f
+            f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+            f66ddfffffeeeffeffeeeeeffeedb46e
+            .c66ddd4effffffeeeeeffff4ddb46e.
+            .fc6b4dddddddddddddddddddb444ee.
+            ..ff6bb444444444444444444444ee..
+            ....ffbbbb4444444444444444ee....
+            ......ffebbbbbb44444444eee......
+            .........fffffffcccccee.........
+            ................................
+            `, SpriteKind.Food)
+        foods.setPosition(randint(20, 140), randint(20, 140))
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileDarkGrass2, function (sprite, location) {
     game.over(true)
 })
+let foods: Sprite = null
+let count = 0
 let mySprite = sprites.create(img`
     . . . . f f f f f . . . . . . . 
     . . . f e e e e e f . . . . . . 
@@ -21,24 +64,8 @@ let mySprite = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(mySprite, 150, 150)
 scene.setBackgroundColor(9)
-tiles.setTilemap(tiles.createTilemap(hex`1000100001010101010101010101010101010101010202020101010101010101010101010101010201010102020101010101010103020202020201020101020101010101010101010102020201010202020202010101020101020101010101010201020101010201010201010102020202010101010102020202010101020101020201010101010101020201010201010101010101010101010201010102010101010101010202010102020202020202020201010101020101010102010101010102010101010202020202020101010101020101010101010102010101010102020202040101010101020101010101020101010101010101010101010101010101010101`, img`
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    2 . . . 2 2 2 2 2 2 2 2 2 2 2 2 
-    2 2 2 . 2 2 2 . . 2 2 2 2 2 2 2 
-    . . . . . . 2 . 2 2 . 2 2 2 2 2 
-    2 2 2 2 2 . . . 2 2 . . . . . 2 
-    2 2 . 2 2 . 2 2 2 2 2 2 . 2 . 2 
-    2 2 . 2 2 . 2 2 2 . . . . 2 2 2 
-    2 2 . . . . 2 2 2 . 2 2 . . 2 2 
-    2 2 2 2 2 . . 2 2 . 2 2 2 2 2 2 
-    2 2 2 2 2 . 2 2 2 . 2 2 2 2 2 2 
-    2 . . 2 2 . . . . . . . . . 2 2 
-    2 2 . 2 2 2 2 . 2 2 2 2 2 . 2 2 
-    2 2 . . . . . . 2 2 2 2 2 . 2 2 
-    2 2 2 2 2 . 2 2 2 2 2 . . . . . 
-    2 2 2 2 2 . 2 2 2 2 2 . 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    `, [myTiles.transparency16,sprites.builtin.forestTiles0,sprites.dungeon.darkGroundCenter,sprites.builtin.brick,sprites.castle.tileDarkGrass2], TileScale.Sixteen))
+tiles.setTilemap(tilemap`level1`)
 tiles.placeOnRandomTile(mySprite, sprites.builtin.brick)
 scene.cameraFollowSprite(mySprite)
 info.startCountdown(20)
+music.siren.play()
