1554e42565a9bd070af002b36f2adac3687cc963
MAXCOLES <77454678+MAXCOLES@users.noreply.github.com>
2021-02-04 17:05:47 +0000

    done some more, ive tried to make it so that the levels change but i cant for some reason

--- main.ts
+++ main.ts
@@ -2,33 +2,41 @@ function start_level () {
     scene.setBackgroundColor(randint(3, 7))
     count = 0
     for (let level = 0; level <= 10 + level; level++) {
-        mySprite = sprites.create(img`
-            . . . . f f f f f . . . . . . . 
-            . . . f e e e e e f . . . . . . 
-            . . f d d d d e e e f . . . . . 
-            . c d f d d f d e e f f . . . . 
-            . c d f d d f d e e d d f . . . 
-            c d e e d d d d e e b d c . . . 
-            c d d d d c d d e e b d c . f f 
-            c c c c c d d d e e f c . f e f 
-            . f d d d d d e e f f . . f e f 
-            . . f f f f f e e e e f . f e f 
-            . . . . f e e e e e e e f f e f 
-            . . . f e f f e f e e e e f f . 
-            . . . f e f f e f e e e e f . . 
-            . . . f d b f d b f f e f . . . 
-            . . . f d d c d d b b d f . . . 
-            . . . . f f f f f f f f f . . . 
-            `, SpriteKind.Player)
+    	
     }
-    info.startCountdown(10)
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd2, function (sprite, location) {
+    game.over(true)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     level += 1
+    mySprite = sprites.create(img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . c d f d d f d e e f f . . . . 
+        . c d f d d f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . f f 
+        c c c c c d d d e e f c . f e f 
+        . f d d d d d e e f f . . f e f 
+        . . f f f f f e e e e f . f e f 
+        . . . . f e e e e e e e f f e f 
+        . . . f e f f e f e e e e f f . 
+        . . . f e f f e f e e e e f . . 
+        . . . f d b f d b f f e f . . . 
+        . . . f d d c d d b b d f . . . 
+        . . . . f f f f f f f f f . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(mySprite, 150, 150)
+    scene.setBackgroundColor(8)
+    tiles.setTilemap(tilemap`level3`)
+    tiles.placeOnRandomTile(mySprite, sprites.castle.saplingOak)
+    scene.cameraFollowSprite(mySprite)
+    info.startCountdown(20)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileDarkGrass2, function (sprite, location) {
     game.over(true)
-    info.changeScoreBy(1)
 })
 let level = 0
 let count = 0
